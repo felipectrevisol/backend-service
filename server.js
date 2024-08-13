3b59aa975d4488a15334e9a082cb67258530149d
Felipe C. Trevisol <164936415+felipectrevisol@users.noreply.github.com>
2024-08-13 20:15:39 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import 'dotenv/config'
 import express from 'express';
-import contactHttpApi from './src/Contacts/ContactHttpApi.js';
 import { openDatabaseConnection } from "./src/database.js";
+import contactHttpApi from './src/Contacts/ContactHttpApi.js';
 import phoneNumberHttpApi from './src/Contacts/PhoneNumberHttpApi.js';
 
 (async () => await openDatabaseConnection())();

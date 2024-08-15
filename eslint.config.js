49683dd8c9b63227f4ed9077448353a1cc4a3b8b
Felipe C. Trevisol <felipecantotrevisol@gmail.com>
2024-08-15 15:28:57 -0300

    Eslint configurado para o ambiente nodejs, e adicionado o Dockerfile para configuraçaão futura.

--- eslint.config.js
+++ eslint.config.js
@@ -2,11 +2,13 @@ import js from "@eslint/js";
 
 export default [
     js.configs.recommended,
-
     {
         rules: {
             "no-unused-vars": "warn",
             "no-undef": "warn"
+        },
+        env: {
+            node:true
         }
     }
 ];

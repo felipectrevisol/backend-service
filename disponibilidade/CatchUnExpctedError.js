901208a6a3eba0b1018d108e26660df4bb3ce9e7
Felipe C. Trevisol <felipecantotrevisol@gmail.com>
2024-08-18 18:15:46 -0300

    Desenvolvendo a captura de erros.

--- disponibilidade/CatchUnExpctedError.js
+++ disponibilidade/CatchUnExpctedError.js
@@ -1,7 +1,15 @@
-//#TODO Estuar este casso.
-(() => {
+function catchUnExpectedError() {
     process.on('uncaughtException', (error) => {
         //#TODO adicionar o Sentry aqui: por exemplo.
-        console.log("Houve um erro inesperado!");
+        console.log(`
+            Houve um erro inesperado!\n
+            Message: ${error.message}\n
+            Cause: ${error.cause}\n
+            Code: ${error.code}\n
+            String Error: ${error.toString()}
+        `);
+        process.abort();
     });
-});
+}
+
+export default catchUnExpectedError;
